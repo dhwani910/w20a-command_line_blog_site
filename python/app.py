0bb4e16bec4d0ee0ac682cb9a91b495768e0ef31
unknown <pateldhwani59@gmail.com>
2021-02-16 14:53:44 -0800

    create login functionality

--- python/app.py
+++ python/app.py
@@ -69,14 +69,57 @@ def Select_blog():
        
 
 
+def login():
+    conn  = None
+    cursor = None
 
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        username = input("enter your username: ")
+        password = input("enter your password: ")
+        if username != "" and password != "":
+            try:
+                cursor.execute("SELECT * FROM users WHERE username = ? and password = ? limit 1", [username, password])
+                rows = cursor.fetchall()[0]
+                row_count = cursor.rowcount
+                print(row_count, "user detected")
+                if cursor.rowcount == 1:
+                    id = rows[0]
+                    name = rows[1]
+                    password = rows[2]
+                else:
+                    print("error")
+                    pass    
+
+            except:
+                print("invalid username or password")
+                login()
+            else:
+                pass
+        print(rows)
+    except Exception as ex:
+        print(ex)
+    else:
+        if cursor != None:
+            cursor.close()
+        if conn != None:
+            conn.rollback()
+            conn.close()    
+   
+
+        return rows 
+rows = login()        
 
 
 while True:
-    print("welcome to Dhwani's first blog site!")
+    print("welcome to my first blog site!")
     username = input("username: ")
-    option = input("1. your new post: , 2. see others post: , 3. Exit ")
+    option = input(" 1. your new post: , 2. see others post: , 3. Exit ")
 
+
+
+        
     if option == "1":
        Insert_blog()
     elif option == "2":
